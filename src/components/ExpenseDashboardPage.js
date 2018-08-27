38d583d85c6dc5444580365b959813d9e026169e
Huy <quochuy.tl.bk@gmail.com>
2018-08-27 17:31:32 +0700

    Setup formatting for amount and createdAt

--- src/components/ExpenseDashboardPage.js
+++ src/components/ExpenseDashboardPage.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ExpenseList from './ExpenseList';
 import ExpenseListFilters from './ExpenseListFilters';
-import myImg from '../assets/images/46035.jpg';
 
 const ExpenseDashboardPage = () => (
     <div>
